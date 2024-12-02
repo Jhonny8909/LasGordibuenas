b1b883ac664202d28af302b4117d5bae53217a13
Bl4ke36 <3du4rdo.pv@gmail.com>
2024-12-01 21:56:22 -0600

    carrito demo

---
+++ src/js/carrito.js
@@ -0,0 +1,38 @@
+const $lista = document.querySelector(".carrito")
+const $btn_limpiar = document.querySelector(".btn_limpiar")
+
+let carrito_lista = JSON.parse(window.localStorage.getItem("carrito_lista"));
+
+console.log(carrito_lista)
+
+if(carrito_lista[0] == null){
+    VerCArrito();
+}else{
+    carrito_lista.forEach(VerCArrito);
+}
+
+function VerCArrito(item){
+    if(item == null){
+        $lista.innerHTML = `<li><b>No hay items en el carrito</b></li>`
+    }else{
+        $lista.innerHTML += `<li><div class="container text-center">
+        <div class="row align-items-center">
+            <div class="col">
+                <img src="${item.Image}">
+            </div>
+            <div class="col">
+                <p class = "carrito_text">${item.Name}</p>
+            </div>
+            <div class="col">
+                <p class = "carrito_text number">x${item.Quantity}</p>
+            </div>
+        </div></li>`
+    }
+}
+
+function LimpiarCarrito(){
+    carrito_lista = [];
+    console.log(carrito_lista);
+    window.localStorage.setItem("carrito_lista",JSON.stringify(carrito_lista));
+    VerCArrito();
+}
--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,33 @@ import "./auth-email.js"
 import "./auth-google.js"
 import "./recuperacion.js"
 
+const $btn_carrito = document.querySelectorAll(".carrito");
+
+let carrito_lista = JSON.parse(window.localStorage.getItem("carrito_lista"));
+
+console.log(carrito_lista);
 
 onAuthStateChanged(auth, async (user) => {
     CheckUsuario(user)
 })
 
+$btn_carrito.forEach((btn) => btn.addEventListener("click", function(){ 
+    if(carrito_lista.length != 0){
+        for (let i = 0; i < carrito_lista.length; i++){
+            if(btn.parentElement.parentElement.children[0].textContent == carrito_lista[i].Name){
+                carrito_lista[i].Quantity++
+                break;
+            }
+            else if (i == carrito_lista.length - 1){
+                carrito_lista.push({"Name" : btn.parentElement.parentElement.children[0].textContent, "Price" : btn.parentElement.parentElement.children[2].children[0].textContent, "Quantity": 1, "Image" : btn.parentElement.parentElement.parentElement.children[0].children[0].src});
+                break;
+            }
+        }
+    }
+    else if (carrito_lista.length == 0){
+        carrito_lista.push({"Name" : btn.parentElement.parentElement.children[0].textContent, "Price" : btn.parentElement.parentElement.children[2].children[0].textContent, "Quantity": 1, "Image" : btn.parentElement.parentElement.parentElement.children[0].children[0].src});
+    }
+
+    window.localStorage.setItem("carrito_lista",JSON.stringify(carrito_lista));
+    
+}));
