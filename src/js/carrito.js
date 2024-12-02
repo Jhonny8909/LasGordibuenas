fe66474295e73176ec82f2d25d531b76b75fd458
Bl4ke36 <3du4rdo.pv@gmail.com>
2024-12-01 22:11:18 -0600

    total

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -1,7 +1,9 @@
 const $lista = document.querySelector(".carrito")
+const $total = document.querySelector(".total")
 const $btn_limpiar = document.querySelector(".btn_limpiar")
 
 let carrito_lista = JSON.parse(window.localStorage.getItem("carrito_lista"));
+let total = 0;
 
 console.log(carrito_lista)
 
@@ -14,7 +16,9 @@ if(carrito_lista[0] == null){
 function VerCArrito(item){
     if(item == null){
         $lista.innerHTML = `<li><b>No hay items en el carrito</b></li>`
+        $total.innerHTML = `<b>TOTAL: $0</b>`
     }else{
+        total += item.Price.replace(/^\D+/g, '')* item.Quantity;
         $lista.innerHTML += `<li><div class="container text-center">
         <div class="row align-items-center">
             <div class="col">
@@ -27,7 +31,10 @@ function VerCArrito(item){
                 <p class = "carrito_text number">x${item.Quantity}</p>
             </div>
         </div></li>`
+
+        $total.innerHTML = `<b>TOTAL: $${total}</b>`
     }
+    console.log(total)
 }
 
 function LimpiarCarrito(){
